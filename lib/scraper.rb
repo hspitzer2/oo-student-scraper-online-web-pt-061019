12f3ba470453eec9a4d2307cd461be1aaeecbda3
Hillary Spitzer <hspitzer2@gmail.com>
2019-08-06 22:12:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,13 +3,21 @@ require 'pry'
 
 class Scraper
 
+
   def self.scrape_index_page(index_url)
-    
+    student_hash = []
+    html = Nokogiri::html(open(index_url))
+    html.css(".student-card").collect do |student|
+    hash = {  
+      name: student.css(h4.student-name).text
+      location: student.css(p.student-location).text
+      profile_url: student.css("a").attribute("href").value
+     end
+   student_hash
+ end
+
+    def self.scrape_profile_page(profile_url)
+      
+   end
+
   end
-
-  def self.scrape_profile_page(profile_url)
-    
-  end
-
-end
-
